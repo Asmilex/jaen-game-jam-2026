4dddfc553da6823c9888de7621ae3a8a2b74e5b1
DARKNOMAD\manue <manuelherreracontacto@gmail.com>
2026-02-01 09:11:19 +0100

    Added colored door sound | Fixed issues in all levels

--- Assets/Scripts/Interactive/LevelDoors.cs
+++ Assets/Scripts/Interactive/LevelDoors.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class LevelDoors : MonoBehaviour, IActivable
 {
     private Animator _animator;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip doorAudioClip;
 
     void Start()
     {
-        _animator = GetComponent<Animator>();
+        _animator = this.gameObject.GetComponent<Animator>();
     }
 
     public void Activate()
     {
         _animator.SetTrigger("Open");
+        DoorSound();
     }
 
     public void Deactivate()
     {
         _animator.SetTrigger("Close");
+        DoorSound();
+    }
+
+    private void DoorSound()
+    {
+        audioSource.PlayOneShot(doorAudioClip);
     }
 }
