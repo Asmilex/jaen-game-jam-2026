0849c9418bdf9ec3802adbdfeb2f984f04f794cc
DARKNOMAD\manue <manuelherreracontacto@gmail.com>
2026-02-01 09:22:48 +0100

    Created trigger at scene start to load ending scene o main menu again

--- Assets/Scripts/Menus/TriggerToEndGame.cs
+++ Assets/Scripts/Menus/TriggerToEndGame.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerToEndGame : MonoBehaviour
 {
+    public string sceneToChangeName;
 
     void OnTriggerEnter(Collider other)
     {
-        
+        SceneManager.LoadScene(sceneToChangeName);
     }
 }
