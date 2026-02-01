8a722e916601089ad8a8bffee13e7f60afe947d3
DARKNOMAD\manue <manuelherreracontacto@gmail.com>
2026-02-01 15:16:29 +0100

    Added time counter

--- Assets/Scripts/Menus/GoingToMainMenuAfterCredits.cs
+++ Assets/Scripts/Menus/GoingToMainMenuAfterCredits.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GoingToMainMenuAfterCredits : MonoBehaviour
 {
     public AnimationClip creditsAnimator;
     public AudioSource musicAudioSource;
+    public TMP_Text finalCounterAtCredits;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SetPlayedTimeText();
+
         StartCoroutine(GoingToMainMenuAfterCreditsCoroutine());
 
+
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        
+
         Destroy(player);
         //Destroy(GameController.Instance.gameObject);
     }
@@ -25,5 +30,10 @@ public class GoingToMainMenuAfterCredits : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
 
         Destroy(GameController.Instance.gameObject);
+    }
+
+    public void SetPlayedTimeText()
+    {
+        finalCounterAtCredits.text = "Time played: "+ GameController.Instance.gameObject.GetComponent<GameController>().TimeSpentOnSession();
     }
 }
